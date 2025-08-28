a2b35a4c46ad899422a6ce625698d4e885bb7936
xTrayambak <xtrayambak@gmail.com>
2025-08-28 19:38:15 +0530

    scheisse

--- system/services/ssh.nix
+++ system/services/ssh.nix
@@ -3,13 +3,6 @@
     enable = true;
     settings = {
       X11Forwarding = false; # We don't need this.
-      PrintMotd = true;
-      banner = ''
-        Number 15: Burger King Foot Lettuce
-
-        The last thing you want in your Burger King burger, is someone else's foot fungus.
-        Unfortunately, this might be what you get.
-        			'';
     };
   };
 }
